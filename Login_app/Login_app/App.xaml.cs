5d7b54252be46df2e1ec5100a25bc67ccb4fa856
hp <hp@LAPTOP-Q84RNN2B>
2022-10-06 00:57:02 +0300

    Add project files.

---
+++ Login_app/Login_app/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+
+            MainPage = new LoginUI(); 
+            //MainPage = new OnBoarding(); 
+            
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Login_app/Login_app/OnBoarding.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login_app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OnBoarding : ContentPage
+    {
+        public OnBoarding()
+        {
+            InitializeComponent();
+        }
+
+        //private async void Button_Clicked(object sender,EventArgs e)
+        //{
+        //    await Navigation.PushAsync(new LoginUI());
+        //}
+
+    }
+}
